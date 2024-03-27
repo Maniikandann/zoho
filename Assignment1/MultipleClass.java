5d4f1e8eadb219086d77fd1d263219e357b2ebb1
Maniikandann <manikandanpkl2002@gmail.com>
2024-03-27 14:39:29 +0530

    testing

--- Assignment1/MultipleClass.java
+++ Assignment1/MultipleClass.java
@@ -18,7 +18,6 @@ package Assignment1;
 	    public static void main(String[] args) {
 	        Class1 class1 = new Class1();
 	        Class2 class2 = new Class2();
-
 	        class1.one();
 	        class2.two();
 	    }
